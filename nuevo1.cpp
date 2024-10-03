0afc2ee038d7e67f0773fcc966e7ed710bb59081
XSupremoLucic <98796941+XSupremoLucic@users.noreply.github.com>
2024-10-02 22:46:10 -0300

    Add files via upload

---
+++ nuevo1.cpp
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<HTML style="background-color: red">
+<title>pagina de prueba</title>
+<center>
+<body>
+<h1>hola<br>
+<img src="" border=3><br>
+<img src="" border=3><br>
+<img src="" border=3><br>
+<h2> xd
+</body>
+</center>
+</html>
